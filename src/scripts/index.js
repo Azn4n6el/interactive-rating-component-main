b37fa0fa87daeec3fb79d9e28ee071c6afeae7a3
Angel Lam <anglam08@gmail.com>
2022-12-28 20:54:11 -0500

    finished basic rating page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,36 @@
 (()=>{
-    console.log('hello');
+
+    const [...allButtons] = document.getElementById("rating-scale").getElementsByTagName('button');
+    const formSearch = document.getElementById('rating-form');
+
+    const globalValues = {
+        selectedRating: undefined,
+    }
+
+    const initialize = () => {
+        allButtons.forEach(element => {
+            element.addEventListener('click', onSelectReview);
+        });
+        formSearch.addEventListener('submit', onSubmitForm);
+    }
+
+    function onSelectReview (event){
+        allButtons.forEach(element => {
+            element.removeAttribute('data-selected');
+        })
+        event.target.setAttribute('data-selected', "true");
+        console.log(event);
+    };
+
+    function onSubmitForm (event){
+        event.preventDefault();
+        [...event.target].forEach(element => {
+            if (element.hasAttribute('data-selected')){
+                globalValues.selectedRating = element.value;
+            }
+        })
+        console.log(globalValues.selectedRating);
+    }
+
+    initialize();
 })();
